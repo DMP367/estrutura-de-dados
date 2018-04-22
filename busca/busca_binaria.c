756cd7654a4a9d31d48e1de34b16b17ff357f3ab
Daniel Mendes Pimentel <daniel67.mendes.pimentel@gmail.com>
2018-04-22 18:16:27 -0300

    implementando busca binaria

---
+++ busca/busca_binaria.c
@@ -0,0 +1,9 @@
+#include "busca_binaria.h"
+
+
+int busca_binaria(int* v, size_t size, int chave) {
+	int pos;
+
+
+	return pos;
+}
---
+++ busca/busca_binaria.h
@@ -0,0 +1,9 @@
+#ifndef BUSCA_BINARIA_H_INCLUDED
+#define BUSCA_BINARIA_H_INCLUDED
+
+#include <stdlib.h>
+
+int busca_binaria(int* v, size_t size, int chave);
+
+
+#endif
